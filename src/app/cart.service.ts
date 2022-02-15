13ea9588ed1265767d02681c3fe9cb7c1c39b169
Collin Beane <beane039@morris.umn.edu>
2022-02-14 18:48:21 -0600

    Add Create the shopping Cart Service Step
    Adds code to complete the "Create the shopping cart service" step of Managing data part of angular tutorial.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,12 @@
+import { Injectable } from '@angular/core';
 import { Product } from './products';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
-
   items: Product[] = [];
+/* . . . */
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -16,5 +20,5 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-
+/* . . . */
 }
